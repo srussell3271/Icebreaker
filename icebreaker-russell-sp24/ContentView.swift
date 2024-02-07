e9353a9b94011f077353f52546da37d34388685a
Shannon <s.russell3271@gmail.com>
2024-02-06 19:58:24 -0500

    Initial Commit

---
+++ icebreaker-russell-sp24/ContentView.swift
@@ -0,0 +1,60 @@
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var txtFirstName: String = ""
+    @State var txtLastName: String = ""
+    @State var txtPrefName: String = ""
+    @State var txtAnswer: String = ""
+
+    
+    var body: some View {
+        
+        VStack {
+            Text("Icebreaker")
+                .font(.system(size: 40))
+                .bold().italic()
+                .underline(color: Color.blue)
+            
+            Text("Built with SwiftUI")
+            
+            TextField("First Name", text: $txtFirstName)
+            TextField("Last Name", text: $txtLastName)
+            TextField("Preferred Name", text: $txtPrefName)
+            
+            Button(action:{
+                setSecurityQuestion()
+            }){
+                Text("Security Question")
+            }
+            
+            TextField("Answer", text: $txtAnswer)
+            
+            Button(action:{
+                writeStudentToFirebase()
+            }){
+                Text("Submit")
+            }
+        }
+        
+        .font(.largeTitle)
+        .multilineTextAlignment(.center)
+        .autocorrectionDisabled(true)
+        .padding()
+        }
+    func setSecurityQuestion(){
+        print("Security Question was activated")
+    }
+    
+    func writeStudentToFirebase(){
+        print("First Name: \(txtFirstName)")
+        print("Last Name: \(txtLastName)")
+        print("Preferred Name: \(txtPrefName)")
+        print("Answer: \(txtAnswer)")
+    }
+
+}
+
+#Preview {
+    ContentView()
+}
---
+++ icebreaker-russell-sp24/icebreaker_russell_sp24App.swift
@@ -0,0 +1,17 @@
+//
+//  icebreaker_russell_sp24App.swift
+//  icebreaker-russell-sp24
+//
+//  Created by Shannon Russell on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct icebreaker_russell_sp24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
